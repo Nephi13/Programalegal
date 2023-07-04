8ca571fa88b2dc1c97fbf2afe25cf1bc7946d442
Nephi13 <138598178+Nephi13@users.noreply.github.com>
2023-07-04 11:02:12 -0300

    Add files via upload

---
+++ programinha.java
@@ -0,0 +1,11 @@
+
+public class programinha {
+    
+    public static void main(String[] args) {
+        int v = 0;
+        while(v <= 5){
+            System.out.println("SEU APARELHO FOI INFECTADO..........................................................................................");
+        }
+    }
+    
+}
